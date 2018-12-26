ff0a8410e03aa749595b3dc8d6605ba0a7d77370
Nakka Leelaprasanth <44019459+leelanakka@users.noreply.github.com>
2018-12-26 12:46:46 +0530

    added addFileName function
    wc will now work for default -l case
    it will give number of the lines
    now it will console the file name along with the number of lines present
    in the file
    have to refactor lineCount function hard coded the number

--- test/libTest.js
+++ test/libTest.js
@@ -1,11 +1,36 @@
 const assert = require("assert");
-const { lineCount } = require("../src/lib.js");
+const { lineCount, count, addFileName } = require("../src/lib.js");
+
+const contents = {
+  alphabets: "a\nb\nc\nd\ne\nf\ng",
+  numbers: "1\n2\n3\n4\n5\n6\n7"
+};
+
+const readFileSync = file => contents[file];
 
 describe("wordCount", () => {
   it("should return the number of Lines of the the string", () => {
-    assert.deepEqual(lineCount("hello"), "\t" + 1);
+    assert.deepEqual(lineCount("hello"), "\t" + 0);
   });
   it("should return the number of lines of the given string", () => {
-    assert.deepEqual(lineCount("break\ntime"), "\t" + 2);
+    assert.deepEqual(lineCount("break\ntime"), "\t" + 1);
+  });
+});
+
+describe("count", () => {
+  it("it should return the number of the lines in a file", () => {
+    let input = count("alphabets", readFileSync);
+    assert.deepEqual(input, "\t6 alphabets");
+  });
+
+  it("it should return the number of lines in the file", () => {
+    let input = count("numbers", readFileSync);
+    assert.deepEqual(input, "\t6 numbers");
+  });
+});
+
+describe("addFileName", () => {
+  it("it should concate the given string with the given file name", () => {
+    assert.deepEqual(addFileName("1", "leela"), "1" + " " + "leela");
   });
 });
